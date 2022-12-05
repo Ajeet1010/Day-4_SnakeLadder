13a60865fd7d74404d7b8ce0bca7c927200661de
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-06 01:22:46 +0530

    Start

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Snake
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose the option to perform the operation:-  1) Start \n 2) \n 3) \n4)  \n 5)  \n" +
+                        " 6)  \n 7)  \n " +
+                        "Enter the no to operate: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Start start = new Start();
+                        start.Dice();
+                        break;
+
+
+                }
+            }
+        }
+
+    }
+}
---
+++ SnakeLadder/SnakeLadder/Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Start
+    {
+        public void Dice()
+        {
+            int PositionP1 = 0;
+            int Dice = 0;
+            Console.WriteLine("Game Begin : " + PositionP1);
+        }
+    }
+}
