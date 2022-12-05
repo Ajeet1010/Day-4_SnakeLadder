b361f1470877c2e3709c5807c64531e6abe3d1f7
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-05 23:57:13 +0530

    Attendance

---
+++ EmployeeProblem/EmployeeProblem/Attendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProblem
+{
+    public class Attendance
+    {
+        public void Preabs()
+        {            
+                int isPresent = 1;
+                int isAbsent = 0;
+                Random random = new Random();
+                int empcheck = random.Next(0, 2);
+                if (empcheck == isPresent)
+                {
+                    Console.WriteLine("Employee is present");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is absent");
+                }
+        }
+    }
+}
---
+++ EmployeeProblem/EmployeeProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProblem
+{
+    public class EmployeeProblem
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose the option to perform the operation:-  1) Attendance \n 2) Employee Wage \n 3)Power of 2 \n4) Harmonic \n 5) Factors \n" +
+                    " 6) QueRem \n 7) Swapping \n 8) EvenOdd \n 9) Alphabet \n 10)Largest no\n" +
+                    "Enter the no to operate: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Attendance attendance = new Attendance();
+                        attendance.Preabs();
+                        break;
+
+                }
+            }
+        }
+    }
+}
